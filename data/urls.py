b97d0e06bbbc52f5acb6b221ce9289b546cd2b76
Syoma Lukashenko <samlukashenko@gmail.com>
2025-08-04 11:24:41 +0300

    another_minor_fix

--- data/urls.py
+++ data/urls.py
@@ -1,7 +1,7 @@
 BASE_URL = 'https://foodgram-frontend-1.prakticum-team.ru/'
 SIGN_IN = 'signin'
 SIGN_UP = 'signup'
-RECEIPES = 'receipes'
+RECEIPES = 'recipes'
 
 LOGIN_URL = BASE_URL + SIGN_IN
 CREATE_ACCOUNT_URL = BASE_URL + SIGN_UP
